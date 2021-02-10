4484df4d03a1704d065e8ccc9822350ac7b679e3
Sylvie-B <unsolo@hotmail.fr>
2021-02-10 13:53:46 +0100

    create class Maison

--- classes/Maison.php
+++ classes/Maison.php
@@ -6,5 +6,53 @@ class Maison extends Habitation
     private bool $jardin;
     private int $etages;
     private bool $garage;
+
+    /**
+     * @return bool
+     */
+    public function isJardin(): bool
+    {
+        return $this->jardin;
+    }
+
+    /**
+     * @param bool $jardin
+     */
+    public function setJardin(bool $jardin): void
+    {
+        $this->jardin = $jardin;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEtages(): int
+    {
+        return $this->etages;
+    }
+
+    /**
+     * @param int $etages
+     */
+    public function setEtages(int $etages): void
+    {
+        $this->etages = $etages;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isGarage(): bool
+    {
+        return $this->garage;
+    }
+
+    /**
+     * @param bool $garage
+     */
+    public function setGarage(bool $garage): void
+    {
+        $this->garage = $garage;
+    }
 }
 
