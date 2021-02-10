3a4352c95c47ab68d93b044692366967caf265e9
Sylvie-B <unsolo@hotmail.fr>
2021-02-10 13:56:51 +0100

    create class appart

---
+++ classes/Appartement.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Appartement extends Habitation
+{
+    private bool $garage;
+
+    /**
+     * @return bool
+     */
+    public function isGarage(): bool
+    {
+        return $this->garage;
+    }
+
+    /**
+     * @param bool $garage
+     */
+    public function setGarage(bool $garage): void
+    {
+        $this->garage = $garage;
+    }
+}
+
